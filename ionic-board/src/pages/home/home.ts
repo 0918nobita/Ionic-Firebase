635d8897ad49dc929c3efae139254ba8bd724dfd
0918nobita <nobita.0918@gmail.com>
2018-10-11 17:06:52 +0900

    :art: 必要ないフィールドを削除した

--- ionic-board/src/pages/home/home.ts
+++ ionic-board/src/pages/home/home.ts
@@ -7,16 +7,6 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  post: {
-    userName: string,
-    message: string,
-    createdDate: any
-  } = {
-    userName: 'Kodai Matsumoto',
-    message: 'これはテストメッセージです',
-    createdDate: '10分前'
-  };
-
   posts: { userName: string, message: string, createdDate: any }[] =
       [
         { userName: 'ポプ子', message: 'えいえい、おこった？', createdDate: '10分前' },
