e98bc8961e6cda28923e55c1a7ddf858509c6dcb
0918nobita <nobita.0918@gmail.com>
2018-10-15 02:04:55 +0900

    :sparkles: アカウント登録ページに入力欄とボタンを追加した

--- ionic-board/src/pages/signup/signup.ts
+++ ionic-board/src/pages/signup/signup.ts
@@ -1,25 +1,24 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 
-/**
- * Generated class for the SignupPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-signup',
   templateUrl: 'signup.html',
 })
 export class SignupPage {
+  signup: {
+    email: string;
+    password: string;
+    name: string;
+  } = {
+    email: '',
+    password: '',
+    name: ''
+  };
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController) {}
+
+  goBack() {
+    this.navCtrl.pop();
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SignupPage');
-  }
-
 }
