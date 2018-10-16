6b0a8215ad0a274a8e229dab1911b0560e22803d
0918nobita <nobita.0918@gmail.com>
2018-10-16 18:16:02 +0900

    :sparkles: コメント投稿機能を実装した

--- ionic-board/src/pages/comments/comments.ts
+++ ionic-board/src/pages/comments/comments.ts
@@ -1,25 +1,63 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ToastController, NavParams } from 'ionic-angular';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import * as firebase from 'firebase';
 
-/**
- * Generated class for the CommentsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Post } from '../../app/models/post';
+import { Comment } from '../../app/models/comment';
 
-@IonicPage()
 @Component({
   selector: 'page-comments',
   templateUrl: 'comments.html',
 })
 export class CommentsPage {
+  sourcePost: Post;
+  message: string;
+  comment: Comment;
+  comments: Comment[];
+  commentsCollection: AngularFirestoreCollection<Comment>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+      public navParams: NavParams,
+      private toastCtrl: ToastController,
+      private afAuth: AngularFireAuth,
+      private afStore: AngularFirestore
+  ) {
+    this.sourcePost = navParams.get('post');
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CommentsPage');
+    this.getComments();
   }
 
+  getComments() {
+    this.commentsCollection = this.afStore.collection('comments',
+        ref => ref.where('sourcePostId', '==', this.sourcePost.id)
+                  .orderBy('created', 'desc'));
+    this.commentsCollection.valueChanges().subscribe(data => {
+      this.comments = data;
+    });
+  }
+
+  async addComment() {
+    try {
+      const docRef = await this.afStore.collection('comments').add({
+        userName: this.afAuth.auth.currentUser.displayName,
+        message: this.message,
+        created: firebase.firestore.FieldValue.serverTimestamp(),
+        sourcePostId: this.sourcePost.id
+      });
+      this.toastCtrl.create({
+        message: 'コメントを投稿できました',
+        duration: 3000
+      }).present();
+      this.message = '';
+    } catch (error) {
+      this.toastCtrl.create({
+        message: error,
+        duration: 5000
+      }).present();
+    }
+  }
 }
